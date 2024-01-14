80181836e98b9c5eb0ff103214b002c8227236d0
Juraj Višňovský <1135003+juraj-visnovsky@users.noreply.github.com>
2024-01-14 21:53:45 +0100

    Integrate Dice So Nice! (#6)

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -333,6 +333,8 @@ export class CandelafvttActorSheet extends ActorSheet {
                 speaker: ChatMessage.getSpeaker({ actor: this.actor }),
                 flavor: label,
                 rollMode: game.settings.get('core', 'rollMode'),
+                type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+                rolls: [roll],
             });
             return roll;
         }
